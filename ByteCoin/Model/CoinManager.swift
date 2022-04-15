3904a61e34237ee22ec5be8fe2175b142edd09b1
Piotr Żakieta <piter5331@gmail.com>
2022-04-15 21:44:41 +0200

    create performRequest & parseJSON

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,10 +8,60 @@
 
 import Foundation
 
-struct CoinManager {
+protocol CoinManagerDelegate{
+    func didUpdateCoin(coin: CoinModel)
+    func didFailWithError(error: Error)
+}
+
+struct CoinManager{
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "6D845E4C-A5D2-4042-B5A2-5458264C906F"
+    
+    var delegate: CoinManagerDelegate?
+    
+    func getCoinPrice(for currency: String){
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        performRequest(urlString: urlString)
+        
+    }
+    
+    func performRequest(urlString: String){
+        if let url = URL(string: urlString){
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil{
+                    self.delegate?.didFailWithError(error: error!)
+                    
+                    return
+                }
+                if let safeDate = data{
+                    if let coin = self.parseJSON(coinData: safeDate){
+                        self.delegate?.didUpdateCoin(coin: coin)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(coinData: Data) -> CoinModel?{
+        let decoder = JSONDecoder()
+        do{
+            let decodedData = try decoder.decode(CoinData.self, from: coinData)
+            
+            let base = decodedData.base
+            let quote = decodedData.quote
+            let rate = decodedData.rate
+            
+            let coin = CoinModel(base: base, quote: quote, rate: rate)
+            return coin
+        }catch{
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
